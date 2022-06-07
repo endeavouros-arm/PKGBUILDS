3c0aee6b48b9e2cafd4c734d917129565c124224
endeavouros-arm <muzzle-loader@comcast.net>
2022-06-06 20:42:06 -0600

    update g12_sound.sh PKGBUILD

--- odroid-n2-post-install/g12_sound.sh
+++ odroid-n2-post-install/g12_sound.sh
@@ -43,41 +43,6 @@ echo $card
 # ESS 1969 chipset has 2 PCM channels
   mixer $card PCM,1 100%
 
-# Trident/YMFPCI/emu10k1
-  mixer $card Wave 100%
-  mixer $card Music 100%
-  mixer $card AC97 100%
-  mixer $card Surround 90%
-  mixer $card 'Surround Digital' 90%
-  mixer $card 'Wave Surround' 90%
-  mixer $card 'Duplicate Front' 90%
-  mixer $card 'Sigmatel 4-Speaker Stereo' 90%
-
-# CS4237B chipset:
-  mixer $card 'Master Digital' 100%
-
-# DRC
-  mixer $card 'Dynamic Range Compression' 90%
-
-# Envy24 chips with analog outs
-  mixer $card DAC 100%
-  mixer $card DAC,0 100%
-  mixer $card DAC,1 100%
-
-# some notebooks use headphone instead of master
-  mixer $card Headphone 100%
-  mixer $card Speaker 100%
-  mixer $card 'Internal Speaker' 0% mute
-  mixer $card Playback 100%
-  mixer $card Headphone 100%
-  mixer $card Speaker 100%
-  mixer $card Center 100%
-  mixer $card LFE 100%
-  mixer $card Center/LFE 100%
-
-# Intel P4P800-MX  (Ubuntu bug #5813)
-  mixer $card 'Master Playback Switch' on
-
 # set digital output mixer params
   mixer $card 'IEC958' 100% on
   mixer $card 'IEC958 Output' 100%
@@ -93,38 +58,6 @@ echo $card
   mixer $card 'Analog Front' 100%
   mixer $card 'Analog Rear' 100%
   mixer $card 'Analog Center/LFE' 100%
-
-# ASRock ION 330 (and perhaps others) has 2 IEC958 channels
-  mixer $card IEC958,0 on
-  mixer $card IEC958,1 on
-
-# some ION2 has much more IEC958 channels ...
-  mixer $card IEC958,2 on
-  mixer $card IEC958,3 on
-
-# ASRock ION 330 has Master Front set to 0
-  mixer $card 'Master Front' 100%
-
-# Shuttle XS35GT needs this too
-  mixer $card 'Master',0 100% on
-
-# and this for various Fusion devices like Zotac ZBOX
-  mixer $card 'Front',0 100% on
-
-# NVidia CK804 sound devices
-  mixer $card 'IEC958 Playback AC97-SPSA' 100%
-
-# Allwinner H3 Analog
-  mixer $card 'Line Out' 0db on
-
-# Allwinner A20 Analog
-  mixer $card 'Power Amplifier' 0db
-  mixer $card 'Power Amplifier DAC' on
-  mixer $card 'Power Amplifier Mute' on
-
-# Allwinner A64 Analog
-  mixer $card Headphone 0db on
-  mixer $card 'AIF1 Slot 0 Digital DAC' on
 
 # Amlogic G12 HDMI to PCM0 - OLD CONF
   mixer $card 'FRDDR_A SINK 1 SEL' 'OUT 1'
@@ -146,24 +79,18 @@ echo $card
   mixer $card 'SPDIFOUT SRC SEL' 'IN 1'
   mixer $card 'SPDIFOUT Playback' on
 
+# Amlogic G12 Headphone Jack to PCM2
+  mixer "$card" 'TOACODEC Lane Select' 0
+  mixer "$card" 'TOACODEC OUT EN' on
+  mixer "$card" 'TOACODEC SRC' 'I2S c'
+  mixer "$card" 'ACODEC' dB gain: 0.00, 0.00
+  mixer "$card" 'ACODEC Playback Channel Mode' Stereo
+  mixer "$card" 'ACODEC Left DAC Sel' Left
+  mixer "$card" 'ACODEC Right DAC Sel' Right
+
 # Amlogic GX HDMI and S/PDIF
   mixer $card 'AIU HDMI CTRL SRC' 'I2S'
   mixer $card 'AIU SPDIF SRC SEL' 'SPDIF'
-
-# RockPI 4B Analog
-  mixer $card 'Right Headphone Mixer Right DAC' on
-  mixer $card 'Left Headphone Mixer Left DAC' on
-
-# NanoPC T4 Analog
-  mixer $card 'HPO L' on
-  mixer $card 'HPO R' on
-  mixer $card 'HPOVOL L' on
-  mixer $card 'HPOVOL R' on
-  mixer $card 'HPO MIX HPVOL' on
-  mixer $card 'OUT MIXL DAC L1' on
-  mixer $card 'OUT MIXR DAC R1' on
-  mixer $card 'Stereo DAC MIXL DAC L1' on
-  mixer $card 'Stereo DAC MIXR DAC R1' on
 
 done
 
